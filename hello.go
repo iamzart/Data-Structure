7ab05b2476e468c5d98104331af075e6b0931e39
iamzart <1935317403@qq.com>
2025-01-17 23:43:55 +0800

    借我一用

---
+++ hello.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("姓名\t年龄\t籍贯\t住址\njohn\t12\t河北\t北京")
+	var s string = "beautiful"
+	fmt.Println(s)
+	var re int = 520
+	fmt.Println(re)
+}
